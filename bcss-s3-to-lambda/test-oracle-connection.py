f355bf81ef72ad0b3f95d8969ab8f633ce5cc832
Cameron Hargreaves <cameron.hargreaves1@nhs.net>
2025-02-27 13:35:54 +0000

    make tests pass for commit

--- bcss-s3-to-lambda/test-oracle-connection.py
+++ bcss-s3-to-lambda/test-oracle-connection.py
@@ -6,9 +6,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, True)  # add assertion here
 
     def test_connection_to_oracle(self):
-
+        pass
 
     def test_get_data_from_oracle(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
