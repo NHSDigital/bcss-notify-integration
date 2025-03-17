8a0253011b50c9cfa27ca27239a08277b5cc7057
Steve Laing <steve.laing@gmail.com>
2025-03-17 14:25:51 +0000

    Update recipient message status as we fetch the batch
    We need to set a unique message reference for each recipient in our batch. The message reference will allow us to identify the status of each message in our batch.
    We also need to set a default message status of 'requested' and later a status of 'sent' once we have successfully sent the batch to the communication management API.
    This commit refactors the logic around getting recipients, assigning message references and message statuses and updating Oracle.

--- bcss_s3_to_lambda/batch_processor.py
+++ bcss_s3_to_lambda/batch_processor.py
@@ -9,10 +9,6 @@ logging.basicConfig(
 
 
 class BatchProcessor:
-    """
-    Class responsible for processing batches of participants to notify.
-    """
-
     def __init__(self, batch_id: str, db_config: dict):
         self.batch_id = batch_id
         self.db = OracleDatabase(db_config["dsn"], db_config["user"], db_config["password"])
@@ -22,10 +18,7 @@ class BatchProcessor:
             logging.error("Error connecting to the database: %s", e)
 
     def get_routing_plan_id(self):
-        """
-        Retrieves the next batch ID from the database.
-        """
-        routing_plan_id = self.db.get_next_batch(self.batch_id)
+        routing_plan_id = self.db.get_routing_plan_id(self.batch_id)
 
         if not routing_plan_id:
             logging.error("Failed to fetch routing plan ID.")
@@ -33,87 +26,29 @@ class BatchProcessor:
 
         return routing_plan_id
 
-    def get_participants(self):
-        """
-        Retrieves a list of participants for notification.
-
-        Args:
-            batch_id (str): The batch id for the batch of message to be sent.
-        """
-        participants = []
+    def get_recipients(self):
+        recipients = []
 
         try:
-            participants = self.db.get_set_of_participants(self.batch_id)
-            if not participants:
-                logging.error("Failed to fetch participants.")
-                raise DatabaseFetchError("Failed to fetch participants.")
+            recipients = self.db.get_recipients(self.batch_id)
+            if not recipients:
+                logging.error("Failed to fetch recipients.")
+                raise DatabaseFetchError("Failed to fetch recipients.")
         except oracledb.Error as e:
             logging.error({"error": str(e)})
-        finally:
-            self.db.disconnect()
 
-        return participants
+        for recipient in recipients:
+            recipient.message_reference = self.generate_message_reference()
+            recipient.message_status = "REQUESTED"
+            self.db.update_recipient(recipient)
 
+        return recipients
 
-    def generate_participants_message_reference(self, participants):
-        """
-        Generate and update the message references for participants.
+    def mark_batch_as_sent(self, recipients):
+        for recipient in recipients:
+            recipient.message_status = "SENT"
+            self.db.update_recipient(recipient)
 
-        Args:
-            participants (list[list[any]]): List of participants.
-        """
-        participants_list = []
-
-        for participant in participants:
-            participant_list = list(participant)
-
-            nhs_number = participant_list[0]
-            message_reference = str(uuid.uuid4())
-            while self.check_message_reference_exists(message_reference):
-                logging.info(
-                    "Clash detected for UUID %s . Generating a new one...", {message_reference}
-                )
-                message_reference = str(uuid.uuid4())
-            participant_list[1] = message_reference
-            self.update_participant_message_reference(nhs_number, message_reference)
-            participants_list.append(participant_list)
-
-        return participants_list
-
-    def check_message_reference_exists(self, message_reference: str) -> bool:
-        """
-        Checks if the given MESSAGE_ID exists.
-
-        :param message_reference: The MESSAGE_ID value to check
-        :return: True if the MESSAGE_ID exists, False otherwise
-        """
-        query = "SELECT COUNT(*) FROM v_notify_message_queue WHERE MESSAGE_ID = :message_reference"
-        params = {"message_reference": message_reference}
-
-        try:
-            result = self.db.execute_query(query, params)
-            count = result[0][0] if result else 0
-            return count > 0
-        except Exception as e:
-            logging.error("Error checking MESSAGE_REF: %s", e)
-            raise
-
-    def update_participant_message_reference(
-        self, nhs_number: str, message_reference: str
-    ):
-        """
-        Update the MESSAGE_ID for the given participant.
-
-        :param nhs_number: The NHS_NUMBER of the given participant
-        :param message_reference: The MESSAGE_ID to update
-        """
-        try:
-            self.db.execute_non_query(
-                "UPDATE v_notify_message_queue SET MESSAGE_ID = :message_reference \
-                WHERE NHS_NUMBER = :nhs_number",
-                {"message_reference": message_reference, "nhs_number": nhs_number},
-            )
-        except oracledb.Error as e:
-            logging.error({"error": str(e)})
-        finally:
-            self.db.disconnect()
+    @staticmethod
+    def generate_message_reference():
+        return str(uuid.uuid4())
--- tests/unit/bcss_s3_to_lambda/test_batch_processor.py
+++ tests/unit/bcss_s3_to_lambda/test_batch_processor.py
@@ -1,5 +1,6 @@
 from batch_processor import BatchProcessor
 from oracle_database import OracleDatabase, DatabaseFetchError
+from recipient import Recipient
 import pytest
 from unittest.mock import MagicMock, patch
 import uuid
@@ -23,50 +24,48 @@ def plan_id():
 
 
 @pytest.fixture
-def participants():
-    return [
-        ["0000000000", "message_reference_0"],
-        ["1111111111", "message_reference_1"],
-    ]
+def recipients():
+    return [Recipient(("0000000000",)), Recipient(("1111111111",))]
 
 
 @patch("batch_processor.OracleDatabase", autospec=True)
 class TestBatchProcessor:
-    # Test the get_participants method
-    # We need to mock the OracleDatabase class and its methods but we can still test that they are called correctly.
-    def test_get_participants(self, mock_oracle_database, db_config, batch_id, plan_id, participants):
+    def test_get_recipients(self, mock_oracle_database, db_config, recipients):
+        mock_oracle_database.return_value.get_recipients.return_value = recipients
         subject = BatchProcessor(batch_id, db_config)
-        subject.db.get_set_of_participants = MagicMock(return_value=participants)
+        subject.generate_message_reference = MagicMock(
+            side_effect=["message_reference_0", "message_reference_1"])
 
-        assert len(participants) == 2
+        recipients = subject.get_recipients()
 
-        # Assert that the data was correctly returned.
-        nhs_number, message_reference = participants[0]
-        assert nhs_number == "0000000000"
-        assert message_reference == "message_reference_0"
+        assert len(recipients) == 2
 
-        nhs_number, message_reference = participants[1]
-        assert nhs_number == "1111111111"
-        assert message_reference == "message_reference_1"
+        assert recipients[0].nhs_number == "0000000000"
+        assert recipients[0].message_reference == "message_reference_0"
+        assert recipients[0].message_status == "REQUESTED"
 
-    def test_null_participants(self, mock_oracle_database, db_config, batch_id):
+        assert recipients[1].nhs_number == "1111111111"
+        assert recipients[1].message_reference == "message_reference_1"
+        assert recipients[1].message_status == "REQUESTED"
+
+    def test_null_recipients(self, mock_oracle_database, db_config, batch_id):
         subject = BatchProcessor(batch_id, db_config)
 
-        mock_fetch_participants = subject.db.get_set_of_participants
-        mock_fetch_participants.return_value = None
+        mock_fetch_recipients = subject.db.get_recipients
+        mock_fetch_recipients.return_value = None
 
         with pytest.raises(DatabaseFetchError) as exc_info:
-            subject.get_participants()
+            subject.get_recipients()
 
-        assert str(exc_info.value) == "Failed to fetch participants."
-        assert mock_fetch_participants.call_count == 1
+        assert str(exc_info.value) == "Failed to fetch recipients."
+        assert mock_fetch_recipients.call_count == 1
 
     def test_get_routing_plan_id(self, mock_oracle_database, db_config, batch_id):
         subject = BatchProcessor(batch_id, db_config)
 
         plan_id = str(uuid.uuid4())
 
-        mock_fetch_routing_plan_id = subject.db.get_next_batch
+        mock_fetch_routing_plan_id = subject.db.get_routing_plan_id
         mock_fetch_routing_plan_id.return_value = plan_id
 
         routing_plan_id = subject.get_routing_plan_id()
@@ -79,7 +78,7 @@ class TestBatchProcessor:
 
         plan_id = None
 
-        mock_fetch_routing_plan_id = subject.db.get_next_batch
+        mock_fetch_routing_plan_id = subject.db.get_routing_plan_id
         mock_fetch_routing_plan_id.return_value = plan_id
 
         with pytest.raises(DatabaseFetchError) as exc_info:
