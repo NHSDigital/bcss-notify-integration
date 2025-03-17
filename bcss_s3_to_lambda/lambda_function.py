d8658be68d6bded70e07b89a09f0072b452e26c6
Steve Laing <steve.laing@gmail.com>
2025-03-17 15:39:48 +0000

    Update recipient message status on successful batch message request
    We should update the message status of each recipient in the batch if the request to Communication Management API was successful.

--- bcss_s3_to_lambda/lambda_function.py
+++ bcss_s3_to_lambda/lambda_function.py
@@ -75,14 +75,15 @@ def lambda_handler(_event: dict, _context: object) -> None:
 
     routing_plan_id = batch_processor.get_routing_plan_id()
 
-    participants = batch_processor.get_participants()
-    logger.info("participants:\n%s", participants)
+    recipients = batch_processor.get_recipients()
+    logger.info("recipients:\n%s", recipients)
 
     response = CommunicationManagement().send_batch_message(
-        batch_id, routing_plan_id, participants
+        batch_id, routing_plan_id, recipients
     )
 
     if response.status_code == 201:
         logger.info("Batch message sent successfully.")
+        batch_processor.mark_batch_as_sent(recipients)
     else:
         logger.error("Failed to send batch message. Status code: %s", response.status_code)
--- tests/unit/bcss_s3_to_lambda/test_lambda_handler.py
+++ tests/unit/bcss_s3_to_lambda/test_lambda_handler.py
@@ -1,8 +1,10 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
 from lambda_function import (
         lambda_handler, initialise_logger, secrets_client,
         get_secret, db_config
 )
+from recipient import Recipient
+
 import boto3
 import logging
 
@@ -10,35 +12,47 @@ import logging
 @patch("lambda_function.generate_batch_id", return_value="b3b3b3b3-b3b3-b3b3b3b3-b3b3b3b3b3b3")
 @patch("lambda_function.CommunicationManagement", autospec=True)
 @patch("lambda_function.BatchProcessor", autospec=True)
-def test_lambda_handler(mock_bcss_notify_batch_processor, mock_communication_management, generate_batch_id, monkeypatch):
+def test_lambda_handler(mock_batch_processor, mock_communication_management, generate_batch_id, monkeypatch):
     monkeypatch.setenv("host", "host")
     monkeypatch.setenv("port", "port")
     monkeypatch.setenv("sid", "sid")
-    secrets_client = MagicMock()
+    secrets_client = Mock()
     secrets_client.get_secret_value.return_value = {"SecretString": '{"username": "user", "password": "pass"}'}
-    boto3.client = MagicMock(return_value=secrets_client)
+    boto3.client = Mock(return_value=secrets_client)
 
-    mock_bcss_notify_batch_processor.get_routing_plan_id.return_value = "c2c2c2c2-c2c2-c2c2c2c2-c2c2c2c2c2c2"
-    mock_bcss_notify_batch_processor.get_participants.return_value = [{"nhs_number": "1234567890"}]
+    recipient = Recipient(("1234567890", "message_reference_0", "REQUESTED"))
+    routing_plan_id = "c2c2c2c2-c2c2-c2c2c2c2-c2c2c2c2c2c2"
+
+    mock_batch_processor.return_value.get_routing_plan_id = Mock(return_value=routing_plan_id)
+    mock_batch_processor.return_value.get_recipients = Mock(return_value=[recipient])
+
+    mock_response = Mock()
+    mock_response.status_code = 201
+    mock_communication_management.return_value.send_batch_message = Mock(return_value=mock_response)
 
     lambda_handler({}, {})
 
-    mock_bcss_notify_batch_processor.assert_called_once_with(
+    mock_batch_processor.assert_called_once_with(
         "b3b3b3b3-b3b3-b3b3b3b3-b3b3b3b3b3b3",
         {"dsn": "host:port/sid", "user": "user", "password": "pass"}
     )
-    mock_bcss_notify_batch_processor.return_value.get_routing_plan_id.assert_called_once()
-    mock_bcss_notify_batch_processor.return_value.get_participants.assert_called_once()
+    mock_batch_processor.return_value.get_routing_plan_id.assert_called_once()
+    mock_batch_processor.return_value.get_recipients.assert_called_once()
 
     mock_communication_management.assert_called_once()
-    mock_communication_management.return_value.send_batch_message.assert_called_once()
+    mock_communication_management.return_value.send_batch_message.assert_called_once_with(
+        "b3b3b3b3-b3b3-b3b3b3b3-b3b3b3b3b3b3",
+        routing_plan_id,
+        [recipient]
+    )
+    mock_batch_processor.return_value.mark_batch_as_sent.assert_called_once_with([recipient])
 
 
 def test_initialise_logger():
-    mock_logger = MagicMock()
-    mock_stream_handler = MagicMock()
-    logging.getLogger = MagicMock(return_value=mock_logger)
-    logging.StreamHandler = MagicMock(return_value=mock_stream_handler)
+    mock_logger = Mock()
+    mock_stream_handler = Mock()
+    logging.getLogger = Mock(return_value=mock_logger)
+    logging.StreamHandler = Mock(return_value=mock_stream_handler)
 
     logger = initialise_logger()
     assert logger == mock_logger
@@ -48,16 +62,16 @@ def test_initialise_logger():
 
 
 def test_secrets_client(monkeypatch):
-    boto3.client = MagicMock()
+    boto3.client = Mock()
     monkeypatch.setenv("region_name", "us-east-1")
 
     assert secrets_client() == boto3.client("secretsmanager", region_name="us-east-1")
 
 
 def test_get_secret():
-    secrets_client = MagicMock()
+    secrets_client = Mock()
     secrets_client.get_secret_value.return_value = {"SecretString": '{"username": "user", "password": "pass"}'}
-    boto3.client = MagicMock(return_value=secrets_client)
+    boto3.client = Mock(return_value=secrets_client)
 
     assert get_secret("test") == {"username": "user", "password": "pass"}
 
