1466bb405d9fbe21e06d4968fe658ebf8edda92b
Steve Laing <steve.laing@gmail.com>
2025-04-07 12:31:59 +0100

    Assert that we send the correct payload

--- tests/integration/test_batch_notification_processor_updates_message_queue.py
+++ tests/integration/test_batch_notification_processor_updates_message_queue.py
@@ -105,3 +105,39 @@ def test_batch_notification_processor_updates_message_queue(batch_id, recipient_
     for idx, result in enumerate(results):
         recipient = recipient_data[idx]
         assert result == (recipient[0], recipient[1], "SENDING")
+
+
+def test_batch_notification_processor_payload(batch_id, recipient_data):
+    lambda_function.generate_batch_id = Mock(return_value=batch_id)
+    secrets_client = Mock()
+    secret_string = json.dumps({
+        "username": os.getenv("DATABASE_USER"),
+        "password": os.getenv("DATABASE_PASSWORD")
+    })
+    secrets_client.get_secret_value.return_value = {"SecretString": secret_string}
+    boto3.client = Mock(return_value=secrets_client)
+    message_references = [r[1] for r in recipient_data]
+
+    with patch("lambda_function.BatchProcessor.generate_message_reference", side_effect=message_references):
+
+        seed_message_queue(batch_id, recipient_data)
+
+        with requests_mock.Mocker() as rm:
+            adapter = rm.post(
+                f"{os.getenv('COMMGT_BASE_URL')}/api/message/batch",
+                status_code=201,
+                json={"data": {"id": "batch_id"}},
+            )
+
+            lambda_function.lambda_handler({}, {})
+
+        assert adapter.called
+        assert adapter.call_count == 1
+        response_json = adapter.last_request.json()
+
+        assert response_json["data"]["attributes"]["messageBatchReference"] == batch_id
+        assert response_json["data"]["attributes"]["routingPlanId"] == "c3f31ae4-1532-46df-b121-3503db6b32d6"
+
+        for idx, msg in enumerate(response_json["data"]["attributes"]["messages"]):
+            assert msg["recipient"]["nhsNumber"] == recipient_data[idx][0]
+            assert msg["messageReference"] == recipient_data[idx][1]
