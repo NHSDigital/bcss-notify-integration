94db1e6928c3d8a1136f3bea09b99fe64ad06079
Steve Laing <steve.laing@gmail.com>
2025-04-08 09:42:23 +0100

    Refactor how we get read messages
    Amend the params we pass to the GET statuses endpoint, fetch data for messages with a read status.

--- message_status_handler/comms_management.py
+++ message_status_handler/comms_management.py
@@ -2,36 +2,16 @@ import os
 import requests
 import logging
 
-BASE_URL = "http://example.com"
-APPLICATION_ID = os.getenv("application_id")
-API_KEY = os.getenv("api_key")
-SECRET = f"{APPLICATION_ID}.{API_KEY}"
 
-def get_statuses(
-    batch_reference: str,
-    nhs_number: str
-) -> requests.Response:
-
-    headers = {
-        "x-api-key": API_KEY,
-        "api-host": APPLICATION_ID
-    }
-
-    params = {
-        "batchReference": batch_reference,
-        "nhsNumber": nhs_number
-    }
-
-    url = f"{BASE_URL}/statuses"
-
+def get_read_messages(batch_reference: str) -> dict:
     response = requests.get(
-        url,
-        headers=headers,
-        params= params,
+        f"{os.getenv('COMMGT_BASE_URL')}/api/statuses",
+        headers={"x-api-key": os.getenv("API_KEY")},
+        params={"batchReference": batch_reference, "status": "read"},
         timeout=10
     )
 
     if response.status_code != 201:
-        logging.error("Failed to get batch message: %s ", response.json())
+        logging.error("Failed to fetch read messages: %s ", response.json())
 
-    return response
+    return response.json()
