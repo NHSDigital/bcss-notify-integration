f36f1d28f983d8154dd7a863be861d6890c954e9
Tom Smith <tom.smith24@nhs.net>
2025-04-10 14:39:19 +0100

    Added schema to conftest and validation into integration test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -33,7 +33,7 @@ class Helpers:
         conn = oracledb.connect(
             user=os.getenv("DATABASE_USER"),
             password=os.getenv("DATABASE_PASSWORD"),
-            dsn=f"{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_SID')}"
+            dsn=f"{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_SID')}",
         )
         try:
             yield conn.cursor()
@@ -43,18 +43,23 @@ class Helpers:
     @staticmethod
     def seed_message_queue(batch_id, recipient_data):
         secrets_client = Mock()
-        secret_string = json.dumps({
-            "username": os.getenv("DATABASE_USER"),
-            "password": os.getenv("DATABASE_PASSWORD")
-        })
+        secret_string = json.dumps(
+            {
+                "username": os.getenv("DATABASE_USER"),
+                "password": os.getenv("DATABASE_PASSWORD"),
+            }
+        )
         secrets_client.get_secret_value.return_value = {"SecretString": secret_string}
         boto3.client = Mock(return_value=secrets_client)
 
         with Helpers.cursor() as cur:
-            cur.execute("""
+            cur.execute(
+                """
                 INSERT INTO notify_message_batch (batch_id, message_definition_id, batch_status)
                 VALUES (:batch_id, 1, 'new')
-            """, batch_id=batch_id)
+            """,
+                batch_id=batch_id,
+            )
             for recipient in recipient_data:
                 cur.execute(
                     """
@@ -64,7 +69,7 @@ class Helpers:
                     ) VALUES (:nhs_number, :message_reference, 11197, 1, 'new', 1, 1, 1)
                     """,
                     nhs_number=recipient[0],
-                    message_reference=recipient[1]
+                    message_reference=recipient[1],
                 )
 
             cur.connection.commit()
@@ -72,7 +77,9 @@ class Helpers:
     @staticmethod
     def call_get_next_batch(batch_id):
         with Helpers.cursor() as cur:
-            cur.callfunc("PKG_NOTIFY_WRAP.f_get_next_batch", oracledb.STRING, [batch_id])
+            cur.callfunc(
+                "PKG_NOTIFY_WRAP.f_get_next_batch", oracledb.STRING, [batch_id]
+            )
             cur.connection.commit()
 
 
@@ -87,3 +94,47 @@ def reset_db(helpers):
         cur.execute("TRUNCATE TABLE notify_message_queue")
         cur.execute("TRUNCATE TABLE notify_message_batch")
         cur.connection.commit()
+
+
+@pytest.fixture()
+def request_schema():
+    schema = {
+        "type": "object",
+        "properties": {
+            "data": {
+                "type": "object",
+                "properties": {
+                    "routingPlanId": {"type": "string"},
+                    "messageBatchReference": {"type": "string"},
+                    "messages": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "recipient": {
+                                    "type": "object",
+                                    "properties": {"nhsNumber": {"type": "string"}},
+                                    "required": ["nhsNumber"],
+                                },
+                                "messageReference": {"type": "string"},
+                                "personalisation": {"type": "object"},
+                                "originator": {
+                                    "type": "object",
+                                    "properties": {
+                                        "odsCode": {"type": "string"},
+                                    },
+                                },
+                            },
+                            "required": ["recipient", "messageReference"],
+                        },
+                    },
+                },
+                "required": [
+                    "routingPlanId",
+                    "messageBatchReference",
+                    "messages",
+                ],
+            }
+        },
+    }
+    return schema
