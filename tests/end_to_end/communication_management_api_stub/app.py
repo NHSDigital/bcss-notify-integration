8dadcba48ed33daa16a9864aa338e3f9a442af9a
Steve Laing <steve.laing@gmail.com>
2025-04-10 17:01:33 +0100

    WIP call lambda functions from test

--- tests/end_to_end/communication_management_api_stub/app.py
+++ tests/end_to_end/communication_management_api_stub/app.py
@@ -110,4 +110,4 @@ def validate_with_schema(data: dict):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
--- tests/end_to_end/runner/test_batch_processor_schedules_message_status_handler.py
+++ tests/end_to_end/runner/test_batch_processor_schedules_message_status_handler.py
@@ -1,2 +1,13 @@
-def test_batch_processor_schedules_message_status_handler():
-    assert True
+import logging
+import requests
+
+
+def test_batch_processor_schedules_message_status_handler(batch_id, helpers, recipient_data):
+    helpers.seed_message_queue(batch_id, recipient_data)
+    trigger_batch_notifications(batch_id)
+
+
+def trigger_batch_notifications(batch_id):
+    import time; time.sleep(25)
+    response = requests.post("http://localhost:9000/2015-03-31/functions/function/invocations", {"batch_id": batch_id})
+    logging.info("Response: %s", response.json())
