bd65dcb858cad599f76244861f9c71066674c8c8
Steve Laing <steve.laing@gmail.com>
2025-04-17 18:40:09 +0100

    Add inline zipfile null_resource
    We might want to revisit this as apply doesn't work consistently because of source_code_hash computation.

--- terraform/modules/message_status_handler/main.tf
+++ terraform/modules/message_status_handler/main.tf
@@ -1,14 +1,30 @@
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  output_path = "${path.module}/lambda_function.zip"
-  source_dir  = "${path.module}/../../../message_status_handler/"
+resource "null_resource" "zipfile" {
+  provisioner "local-exec" {
+    command     = <<EOT
+      mkdir build
+      cp ../../../message_status_handler/*.py build/
+      cp -r $(pipenv --venv)/lib/python3.11/site-packages/* build/
+      cd build
+      rm -rf __pycache__
+      rm -rf _pytest
+      chmod -R 644 $(find . -type f)
+      chmod -R 755 $(find . -type d)
+      zip -r ../lambda_function.zip * -x *.zip
+      cd ..
+      rm -rf build
+    EOT
+    working_dir = path.module
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 }
 resource "aws_lambda_function" "message_status_handler" {
   function_name    = "${var.team}-${var.project}-message-status-handler-${var.environment}"
   handler          = "scheduled_lambda_function.lambda_handler"
   runtime          = "python3.12"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = "${path.module}/lambda_function.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
   role             = var.message_status_handler_lambda_role_arn
 
   timeout     = 300
