0ebeed434f2fe62093f32360b68f9a75cfe339c7
Steve Laing <steve.laing@gmail.com>
2025-05-07 10:43:43 +0100

    Add common terraform files
    This makes a slight change to backend.tf as dynamodb_table is a deprecated field.

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "bcss-terraform-nonprod-iac"
+    key    = "bcss/infrastructure/communication-management/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
