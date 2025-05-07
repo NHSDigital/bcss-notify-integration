0747494f562fc59ffaa9a1b7be1ae296105bf76a
Steve Laing <steve.laing@gmail.com>
2025-05-07 10:42:07 +0100

    Remove environment dirs

--- terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bcss-terraform-nonprod-iac"
-    key            = "bcss/infrastructure/communication-management/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "bcss-comms-manager-terraform-lock-dev"
-  }
-}
-
